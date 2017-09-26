3804a6efba8b7476ec7c7705ce70116170989c8c
Michal Pandy <michal.pandy@shopify.com>
2017-09-25 22:12:46 -0400

    auto-update push_token

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,9 +2,14 @@ class Device < ApplicationRecord
   has_many :calls, dependent: :destroy
 
   def self.get_device(info_hash)
-    device = find_or_create_by(info_hash.except(:user_agent))
+    device = find_or_create_by(device_uuid: info_hash[:device_uuid])
+
+    # auto-update push_token and user_agent
     unless device.user_agent == info_hash[:user_agent]
       device.update_attribute(:user_agent, info_hash[:user_agent])
+    end
+    unless device.push_token == info_hash[:push_token]
+      device.update_attribute(:push_token, info_hash[:push_token])
     end
     device
   end
